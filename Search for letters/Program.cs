5d29d48de168a89329be43681b0fb5eb7535a229
HenryOrtizAjcuc <heoa100@gmail.com>
2020-05-19 21:43:41 -0600

    first commit, search for letters

---
+++ Search for letters/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Search_for_letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Change("a   **&  cZ"));
+            Console.ReadKey();
+        }
+        public static string Change(string input)
+        {
+            // ...            
+            var cadena = input.ToUpper().ToList().OrderBy(x => x).Distinct();
+
+
+            int posidicon = 65;
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (var item in cadena)
+            {
+                if (item >= 65 && item <= 90)
+                {
+                    while (item > posidicon)
+                    {
+                        output.Append("0");
+                        posidicon++;
+                    }
+                    output.Append("1");
+                    posidicon++;
+                }
+            }
+
+            return output.ToString();
+        }
+    }
+}
